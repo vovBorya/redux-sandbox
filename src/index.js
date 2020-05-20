49d30f852237a82b173e94d1dbd83559a95bfb4a
vovBorya <vovbor02@gmail.com>
2020-05-20 12:10:32 +0300

    ui for redux

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const reducer = (state = 0, action) => {
   switch (action.type) {
     case 'INC':
       return state + 1;
-
+    case 'DEC':
+      return state - 1;
     default:
       return state;
   }
@@ -12,9 +13,21 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
-store.subscribe(() => {
-  console.log(store.getState())
-})
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+document.getElementById('inc')
+  .addEventListener('click', () => {
+    store.dispatch({type: 'INC'})
+  })
+
+document.getElementById('dec')
+  .addEventListener('click', () => {
+    store.dispatch({type: 'DEC'})
+  })
+
+const updateStore = () => {
+  document
+    .getElementById('counter')
+    .innerHTML = store.getState();
+}
+
+store.subscribe(updateStore)
